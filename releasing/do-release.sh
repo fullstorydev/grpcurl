a9a2b5dc5983acbf45e7493d923e08a7ae2832d2
namrata-ibm <namrata.bhave@ibm.com>
2021-09-23 11:16:50 +0000

    Addressing review comments

--- releasing/do-release.sh
+++ releasing/do-release.sh
@@ -41,13 +41,15 @@ $PREFIX git checkout go.mod go.sum
 $PREFIX docker login
 echo "$VERSION" > VERSION
 
-# Enable qemu and binfmt support
+# Docker Buildx support is included in Docker 19.03
+# Below step installs emulators for different architectures on the host
+# This enables running and building containers for below architectures mentioned using --platforms
 $PREFIX docker run --privileged --rm tonistiigi/binfmt:qemu-v6.1.0 --install all
 # Create a new builder instance
 export DOCKER_CLI_EXPERIMENTAL=enabled
 $PREFIX docker buildx create --use --name multiarch-builder
 # push to docker hub, both the given version as a tag and for "latest" tag
-$PREFIX docker buildx build --platform linux/amd64,linux/s390x --tag fullstorydev/grpcurl:${VERSION} --tag fullstorydev/grpcurl:latest --push --progress plain --no-cache .
+$PREFIX docker buildx build --platform linux/amd64,linux/s390x,linux/arm64 --tag fullstorydev/grpcurl:${VERSION} --tag fullstorydev/grpcurl:latest --push --progress plain --no-cache .
 rm VERSION
 
 # Homebrew release
